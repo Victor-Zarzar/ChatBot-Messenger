3f67bd2367ecc4fb0390c3363854cb64b933c6d4
Victor-Zarzar <victorzarzar58@gmail.com>
2023-02-09 22:29:46 -0300

    Project Bot

---
+++ ChatBot Messenger/BotResponse.swift
@@ -0,0 +1,22 @@
+//
+//  BotResponse.swift
+//  ChatBot Messenger
+//
+//  Created by Victor Zarzar on 09/02/23.
+//
+
+import Foundation
+
+func getBotResponse(message: String) -> String {
+    let tempMessage = message.lowercased()
+    
+    if tempMessage.contains("hello") {
+        return "Hey there!"
+    } else if tempMessage.contains("goodbye") {
+        return "Talk to you later!"
+    } else if tempMessage.contains("how are you") {
+        return "I'm fine, how about you?"
+    } else {
+        return "That's cool."
+    }
+}
