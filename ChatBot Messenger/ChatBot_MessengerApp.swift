139d4e7d3c4d7ca55116d151c4a9b4a3bd42b523
Victor-Zarzar <victorzarzar58@gmail.com>
2023-02-09 21:10:48 -0300

    Initial Commit

---
+++ ChatBot Messenger/ChatBot_MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatBot_MessengerApp.swift
+//  ChatBot Messenger
+//
+//  Created by Victor Zarzar on 09/02/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatBot_MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
